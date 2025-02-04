81ecbb17b6127dfd859ab71f05d38d2811f8d759
shimbato <shimbatoo@gmail.com>
2025-02-04 11:22:23 +0500

    feat: HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,29 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      // by name
+      if (action.payload === 'up') {
+        return state.sort((a, b) => a.name.toLocaleLowerCase().localeCompare(b.name));
+      } else {
+        return state.sort((a, b) => b.name.toLocaleLowerCase().localeCompare(a.name));
+      }
+      // need to fix
     }
-}
+    case 'check': {
+      const checkAge = state.filter((age) => {
+        if (age.age > action.payload) {
+          return age.age;
+        }
+      });
+      return checkAge;
+    }
+
+    default:
+      return state;
+  }
+};
